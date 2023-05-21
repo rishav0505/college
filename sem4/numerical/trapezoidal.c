082bf032be1bff4c6a789caf9f5fa272bd736a53
Rishav Saha <98244985+rishav0505@users.noreply.github.com>
2023-05-21 22:45:47 +0530

    correction in trapezoidal (lower limit problem.)

--- sem4/numerical/trapezoidal.c
+++ sem4/numerical/trapezoidal.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#define f(x) 1 / (1 + x * x)
+#include <math.h>
+#define f(x) 1 / sqrt(1 + x * x)
 // limit [0 to 1]
 void main()
 {
-    int n = 6;
+    int n = 5;
     float h, x[n + 1], y[n + 1], l1 = 0, l2 = 1, s = 0.0;
     h = (l2 - l1) / n;
     for (int i = 0; i < n + 1; i++)
     {
-        x[i] = s;
+        x[i] = s + l1;
         y[i] = f(x[i]);
         s += h;
     }
